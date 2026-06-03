5bfed3ed1b883d218117ce4c9f757252dd81d6c8
Leonard <schuhleonardl@gmail.com>
2026-06-03 18:40:52 +0200

    search 2

--- javaScript/toolsCollection.js
+++ javaScript/toolsCollection.js
@@ -3,78 +3,44 @@ export const tools = [
         id: "zahlen-analyse",
         title: "Zahlen Analyse",
         category: "algebra",
-        image: "./pictures/51R9beEdSfL.jpg",
-        url: "/tools/zahlen-analyse/index.html",
-        tags: [
-            "zahlen",
-            "analyse",
-            "algebra",
-            "mathematik"
-        ]
+        image: "/pictures/51R9beEdSfL.jpg",
+        url: "/MathVerse-app/Tools/Zahlenanalyse/zahlenAnalyse.html",
+        tags: ["zahlen", "analyse", "algebra", "mathematik"]
     },
 
     {
         id: "zahlensystem-umrechner",
         title: "Zahlensystem Umrechner",
         category: "zahlensysteme",
-        image: "./pictures/ZahlensystemeUmwandeln Temp1.jpg",
-        url: "/tools/zahlensystem-umrechner/index.html",
-        tags: [
-            "zahlensystem",
-            "umrechner",
-            "dual",
-            "binär",
-            "hex",
-            "oktal",
-            "dezimal"
-        ]
+        image: "/pictures/ZahlensystemeUmwandeln Temp1.jpg",
+        url: "/MathVerse-app/Tools/Zahlensystemumrechner/zsystUmrechner.html", // Pfad an MathVerse-app angepasst
+        tags: ["zahlensystem", "umrechner", "dual", "binär", "hex", "oktal", "dezimal"]
     },
 
     {
         id: "zahlensystem-rechner",
         title: "Zahlensystem Rechner",
         category: "zahlensysteme",
-        image: "./pictures/beispiel-addition.png",
-        url: "/tools/zahlensystem-rechner/index.html",
-        tags: [
-            "zahlensystem",
-            "rechner",
-            "addition",
-            "subtraktion",
-            "multiplikation",
-            "division",
-            "dual"
-        ]
+        image: "/pictures/beispiel-addition.png",
+        url: "/MathVerse-app/Tools/Zahlensystemrechner/zsystRechner.html", // Pfad an MathVerse-app angepasst
+        tags: ["zahlensystem", "rechner", "addition", "subtraktion", "multiplikation", "division", "dual"]
     },
 
     {
         id: "einheiten-umrechner",
         title: "Einheiten Umrechner",
         category: "einheiten",
-        image: "./pictures/einheiten.jpg",
-        url: "/tools/einheiten-umrechner/index.html",
-        tags: [
-            "umrechner",
-            "einheiten",
-            "länge",
-            "gewicht",
-            "temperatur",
-            "zeit"
-        ]
+        image: "/pictures/einheiten.jpg",
+        url: "/MathVerse-app/Tools/Einheiten Umrechner/einheitenUmrechner.html", // Pfad an MathVerse-app angepasst
+        tags: ["umrechner", "einheiten", "länge", "gewicht", "temperatur", "zeit"]
     },
 
     {
         id: "prozent-rechner",
         title: "Prozent Rechner",
         category: "arithmetik",
-        image: "./pictures/prozent.jpg",
-        url: "/tools/prozent-rechner/index.html",
-        tags: [
-            "prozent",
-            "prozentrechnung",
-            "rabatt",
-            "mehrwertsteuer",
-            "rechner"
-        ]
+        image: "/pictures/prozent.jpg",
+        url: "/MathVerse-app/Tools/Prozentrechner/prozentRechner.html", // Pfad an MathVerse-app angepasst
+        tags: ["prozent", "prozentrechnung", "rabatt", "mehrwertsteuer", "rechner"]
     }
 ];
