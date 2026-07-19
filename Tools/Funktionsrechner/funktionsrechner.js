2b043b329acf66ca6dd8ed00e723391086046526
Leonard <schuhleonardl@gmail.com>
2026-07-19 18:58:58 +0200

    funktionsrechner v1

---
+++ Tools/Funktionsrechner/funktionsrechner.js
@@ -0,0 +1,37 @@
+const toolbarButtons = [
+    {
+        id: "reset",
+        icon: "fa-home",
+        tooltip: "return to center"
+    },
+    {
+        id: "fullscreen",
+        icon: "fa-expand",
+        tooltip: "full screen"
+    },
+    {
+        id: "undo",
+        icon: "fa-expand",
+        tooltip: "undo"
+    },
+    {
+        id: "redo",
+        icon: "fa-expand",
+        tooltip: "redo"
+    },
+    {
+        id: "zoom-in",
+        icon: "fa-expand",
+        tooltip: "zoom-in"
+    },
+    {
+        id: "zoom-out",
+        icon: "fa-expand",
+        tooltip: "zoom-out"
+    },
+    {
+        id: "settings",
+        icon: "fa-cog",
+        tooltip: "settings"
+    }
+];
